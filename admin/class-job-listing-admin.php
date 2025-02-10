839b1aa44576db3fa67f9efea669e169fc0d02e3
Chris Lane Jones <chrislanejones@gmail.com>
2025-02-10 11:34:20 -0500

    Admin Code Error

--- admin/class-job-listing-admin.php
+++ admin/class-job-listing-admin.php
@@ -1,81 +1,88 @@
 <?php
-class Job_Listing_Plugin {
-    private static $instance = null;
+class Job_Listing_Admin {
     private $options;
-
-    public static function get_instance() {
-        if (null === self::$instance) {
-            self::$instance = new self();
-        }
-        return self::$instance;
-    }
 
     public function init() {
         $this->options = get_option('job_listing_settings');
         
-        add_action('elementor/widgets/widgets_registered', [$this, 'register_widgets']);
-        add_action('wp_enqueue_scripts', [$this, 'enqueue_scripts']);
-        add_action('rest_api_init', [$this, 'register_rest_route']);
+        add_action('admin_menu', [$this, 'add_admin_menu']);
+        add_action('admin_init', [$this, 'init_settings']);
     }
 
-    public function register_widgets() {
-        require_once(JLP_PLUGIN_DIR . 'includes/widgets/class-job-listing-widget.php');
-        \Elementor\Plugin::instance()->widgets_manager->register_widget_type(new Job_Listing_Widget());
+    public function add_admin_menu() {
+        add_options_page(
+            'Job Listing Settings',
+            'Job Listing',
+            'manage_options',
+            'job-listing-settings',
+            [$this, 'render_settings_page']
+        );
     }
 
-    public function enqueue_scripts() {
-        wp_enqueue_style(
-            'job-listing-style',
-            JLP_PLUGIN_URL . 'assets/css/job-listing.css',
-            [],
-            JLP_VERSION
+    public function init_settings() {
+        register_setting('job_listing_settings', 'job_listing_settings');
+
+        add_settings_section(
+            'job_listing_main_section',
+            'API Settings',
+            null,
+            'job-listing-settings'
         );
 
-        wp_enqueue_script(
-            'job-listing-script',
-            JLP_PLUGIN_URL . 'assets/js/job-listing.js',
-            ['jquery'],
-            JLP_VERSION,
-            true
+        add_settings_field(
+            'client_url',
+            'API URL',
+            [$this, 'render_client_url_field'],
+            'job-listing-settings',
+            'job_listing_main_section'
         );
 
-        wp_localize_script('job-listing-script', 'jobListingData', [
-            'ajaxUrl' => rest_url('job-listing/v1/list'),
-            'nonce' => wp_create_nonce('wp_rest')
-        ]);
+        add_settings_field(
+            'api_key',
+            'API Key',
+            [$this, 'render_api_key_field'],
+            'job-listing-settings',
+            'job_listing_main_section'
+        );
     }
 
-    public function register_rest_route() {
-        register_rest_route('job-listing/v1', '/list', [
-            'methods' => 'GET',
-            'callback' => [$this, 'get_jobs_list'],
-            'permission_callback' => '__return_true'
-        ]);
+    public function render_settings_page() {
+        ?>
+        <div class="wrap">
+            <h1>Job Listing Settings</h1>
+            <form method="post" action="options.php">
+                <?php
+                settings_fields('job_listing_settings');
+                do_settings_sections('job-listing-settings');
+                submit_button();
+                ?>
+            </form>
+        </div>
+        <?php
     }
 
-    public function get_jobs_list() {
-        try {
-            $client_url = isset($this->options['client_url']) 
-                ? $this->options['client_url'] 
-                : 'https://api.ashbyhq.com/jobBoard.list';
-            
-            $api_key = isset($this->options['api_key']) ? $this->options['api_key'] : '';
+    public function render_client_url_field() {
+        $value = isset($this->options['client_url']) ? $this->options['client_url'] : '';
+        ?>
+        <input type="text" 
+               name="job_listing_settings[client_url]" 
+               value="<?php echo esc_attr($value); ?>" 
+               class="regular-text"
+               placeholder="https://api.ashbyhq.com/jobBoard.list"
+        />
+        <p class="description">Enter your API endpoint URL</p>
+        <?php
+    }
 
-            $headers = ['accept' => 'application/json'];
-            if (!empty($api_key)) {
-                $headers['Authorization'] = 'Bearer ' . $api_key;
-            }
-
-            $client = new \GuzzleHttp\Client();
-            $response = $client->request('POST', $client_url, [
-                'headers' => $headers,
-            ]);
-
-            $body = json_decode($response->getBody(), true);
-            return new WP_REST_Response($body, 200);
-
-        } catch (Exception $e) {
-            return new WP_Error('api_error', $e->getMessage(), ['status' => 500]);
-        }
+    public function render_api_key_field() {
+        $value = isset($this->options['api_key']) ? $this->options['api_key'] : '';
+        ?>
+        <input type="password" 
+               name="job_listing_settings[api_key]" 
+               value="<?php echo esc_attr($value); ?>" 
+               class="regular-text"
+        />
+        <p class="description">Enter your API key if required</p>
+        <?php
     }
 }
